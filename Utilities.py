bd5c9b27e98b374369018db2d5e200b7b48271aa
0xOrigin <egyahmed.ezzat120@gmail.com>
2022-09-13 23:29:15 +0200

    Enhance and cleaning the code.

--- Utilities.py
+++ Utilities.py
@@ -3,26 +3,27 @@ import cv2
 
 class Utilities:
     cameras = []
-
+    MAX_CAMERAS = 100
 
     @staticmethod
     def is_camera_available(camera_index):
         cap = cv2.VideoCapture(camera_index)
-        if cap.read()[0]:
-            cap.release()
-            return True
-        else:
-            cap.release()
-            return False
+        result = cap.isOpened()
+        if result:
+            if not cap.read()[0]:
+                result = False
+        cap.release()
+        return result
 
+    @staticmethod
+    def add_camera(camera_index):
+        if Utilities.is_camera_available(camera_index):
+            Utilities.cameras.append(camera_index)
 
     @staticmethod
     def count_cameras():
         i = 0
-        while True:
-            if Utilities.is_camera_available(i):
-                Utilities.cameras.append(i)
-                i += 1
-            else:
-                break
-        return i
+        while i <= Utilities.MAX_CAMERAS:
+            Utilities.add_camera(i)
+            i += 1
+        return len(Utilities.cameras)
